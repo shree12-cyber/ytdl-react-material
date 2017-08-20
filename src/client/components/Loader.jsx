f6be05334cd9f82933f89120977f37f5e705bb85
burnO <228022@student.pwr.edu.pl>
2017-08-20 20:03:09 +0200

    Fix Loader

--- src/client/components/Loader.jsx
+++ src/client/components/Loader.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import { CircularProgress } from 'material-ui/Progress';
 
-export default class Form extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      message: null,
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <CircularProgress size={50} />
-      </div>
-    );
-  }
+export default function Loader() {
+  return <CircularProgress classes="" size={50} />;
 }
