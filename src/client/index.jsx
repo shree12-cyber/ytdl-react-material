4b604d60e6165770bf535a485bf92d0d56c8f23b
burnO <228022@student.pwr.edu.pl>
2017-08-20 18:18:10 +0200

    Remove router

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 window.onload = () => {
-  const app = (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  render(app, document.getElementById('root'));
+  render(<App />, document.getElementById('root'));
 };
--- src/server/index.jsx
+++ src/server/index.jsx
@@ -1,7 +1,6 @@
 import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
 import App from '../client/components/App';
 
 const app = express();
@@ -11,19 +10,10 @@ app.set('views', 'src/server/views');
 app.use(express.static('src/static'));
 
 app.get('*', (req, res) => {
-  const context = {};
-  const markup = (
-    <StaticRouter
-      location={req.url}
-      context={context}
-    >
-      <App />
-    </StaticRouter>
-  );
   res.render('index', {
     title: 'YouTube Downloader',
     author: 'burnO',
-    body: renderToString(markup),
+    body: renderToString(<App />),
   });
 });
 app.listen(3000);
