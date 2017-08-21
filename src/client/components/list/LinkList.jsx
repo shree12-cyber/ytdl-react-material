9e1f5e104e7e58bfc30e61bcd05829d99608a8bd
burnO <228022@student.pwr.edu.pl>
2017-08-21 21:17:52 +0200

    Add sort by bps, remove unknown links

--- src/client/components/list/LinkList.jsx
+++ src/client/components/list/LinkList.jsx
@@ -4,6 +4,21 @@ import PropTypes from 'prop-types';
 import LinkItem from './LinkItem';
 import LinkItemProps from '../props/LinkItemProps';
 
+const getQuality = (linkInfo) => {
+  if (!linkInfo || !linkInfo.resolution) return -1;
+  const index = linkInfo.resolution.indexOf('p');
+  return index > 0 ? parseInt(linkInfo.resolution.substring(0, index), 0) : -1;
+};
+
+const sortVideoList = (linkInfo1, linkInfo2) => {
+  if (getQuality(linkInfo1) < getQuality(linkInfo2)) {
+    return 1;
+  } else if (getQuality(linkInfo1) > getQuality(linkInfo2)) {
+    return -1;
+  }
+  return linkInfo1.audioBitrate < linkInfo2.audioBitrate ? 1 : -1;
+};
+
 const style = {
   maxWidth: '700px',
   marginLeft: 'auto',
@@ -11,21 +26,13 @@ const style = {
   paddingTop: '20px',
 };
 
-const sortQualities = (linkInfo1, linkInfo2) => {
-  const getQuality = (linkInfo) => {
-    if (!linkInfo || !linkInfo.resolution) return -1;
-    const index = linkInfo.resolution.indexOf('p');
-    return index > 0 ? parseInt(linkInfo.resolution.substring(0, index), 0) : -1;
-  };
-  return getQuality(linkInfo1) < getQuality(linkInfo2) ? 1 : -1;
-};
-
 export default function LinkList({ linkInfoList }) {
   return (
     <div style={style}>
       <Grid container>
         {linkInfoList
-          .sort(sortQualities)
+          .filter(linkInfo => linkInfo.container)
+          .sort(sortVideoList)
           .map(linkInfo => <LinkItem key={linkInfo.itag} linkInfo={linkInfo} />)}
       </Grid>
     </div>
